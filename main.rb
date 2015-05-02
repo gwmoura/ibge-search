de1754116e26a454a15eb27a3fb59c3d36a0af85
George Moura <gwmoura@gmail.com>
2015-05-01 21:54:55 -0300

    remove content_type :json

--- main.rb
+++ main.rb
@@ -4,14 +4,12 @@ require 'json'
 require './ibge_parser'
 
 get '/' do
-  content_type :json
   @message = {}
   @message["modo de usar"] = [{"GET" => "/[cidade]"},{"ex" => "/salvador ou /sal"}] 
   erb :index
 end
 
 get '/:city' do |c|
-  content_type :json
   ibge = IBGEParser.new
   @info = ibge.get_city_info(c)
   erb :city_info
