6e2ec190295b32c26fb4700983d233fbbff15e3a
George Moura <gwmoura@gmail.com>
2015-05-18 12:56:14 -0300

    Add config-sample.yml

--- config/warble.rb
+++ config/warble.rb
@@ -1,5 +1,5 @@
 Warbler::Config.new do |config|
   config.dirs     = %w(views)
-  config.includes = FileList["main.rb","ibge_parser.rb"]
+  config.includes = FileList["main.rb","ibge_parser.rb","config-sample.yml"]
   config.jar_name = "ibgesearch"
 end
